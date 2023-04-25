0b3f34748dc2a65006b0a7a762987aebe860c70b
amir.gilani <amir.gilani@CPHHEAD003>
2023-04-25 16:34:53 +0200

    Add project files.

---
+++ Aufgabe-9610/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Quadrieren jeder Zahl
+Schreiben Sie eine Methode mit dem Namen „SquareDigit“ die als Rückgabetyp einen Integer hat 
+und als Parametertyp auch einen Integer Wert. Ihrer Aufgabe ist es dann jede Ziffer der an 
+die Methode übergebenen Integer zu quadrieren, aneinander zu reihen und zurückgeben.
+
+Beispiel
+1.       SquareDigit(9119) => 811181
+
+a.       9² = 81, 1² = 1, 1² = 1, 9² = 81 => 81 1 1 81 => 811181
+2.       SquareDigit(5025) => 250425
+3.       SquareDigit(19501) => 1812501 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufgabe_9610
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
